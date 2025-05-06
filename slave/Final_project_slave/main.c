dbb75a822ab60d3a2a68f3e70765c979a945d837
kstopplecamp <49496979+kstopplecamp@users.noreply.github.com>
2025-05-06 10:30:57 -0600

    final code, modertatly working, but hardware is not

--- slave/Final_project_slave/main.c
+++ slave/Final_project_slave/main.c
@@ -14,6 +14,9 @@ unsigned int diff = 0;
 unsigned int left_right_max = 0;
 unsigned int base_period = 3678;
 volatile bool uart_send_flag = false;
+int Data_Cnt = 0;
+char Packet[] = {0x00, 0x00, 0x00, 0x00};
+unsigned int adc_read_counter = 0;
 
 
 char message[]= "Hello World\n";
@@ -21,17 +24,39 @@ int position;
 int i,j;
 // PD Controller Parameters
 float Kp = 2;     // Position gain (tune as needed)
-float Kd = 0.04;   // Derivative gain (tune as needed)
+float Kd = 0.01;   // Derivative gain (tune as needed)
 
 // Variables to track previous error
 int prev_diff = 0;
 
-int main(void)
+void init_i2c_b0(void){
+        WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
 
+        UCB0CTLW0 |= UCSWRST;                   // Put eUSCI_B0 in SW Reset
+        UCB0CTLW0 |= UCSSEL__SMCLK;             // Choose BRCLK = SMCLK = 1Mhz
+        UCB0BRW = 10;                           // Divide BRCLK by 10 for SCL = 100kHz
+        UCB0CTLW0 |= UCMODE_3;                  // Put into I2C mode
+        UCB0CTLW0 |= UCMST;                     // Put into master mode
+        UCB0CTLW0 |= UCTR;                      // Put into Tx mode
+        UCB0I2CSA = 0x0020;                     // Slave address = 0x20
+        UCB0CTLW1 |= UCASTP_2;                  // Auto STOP when UCB0TBCNT reached
+        UCB0TBCNT = sizeof(Packet);    // # of bytes in packet
 
-{
+        P1SEL1 &= ~BIT3;                        // P1.3 = SCL
+        P1SEL0 |= BIT3;
+        P1SEL1 &= ~BIT2;                        // P1.2 = SDA
+        P1SEL0 |= BIT2;
+
+        PM5CTL0 &= ~LOCKLPM5;                   // Disable low power mode
+
+        UCB0CTLW0 &= ~UCSWRST;                  // Take eUSCI_B0 out of SW Reset
+
+        UCB0IE |= UCTXIE0;                      // Enable I2C Tx0 IR1
+    }
+
+int main(void){
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-
+    init_i2c_b0();
 
     //---------------------------------------------------------------------
     // ADC initialization
@@ -64,7 +89,7 @@ int main(void)
 
     TB0CCR0 = base_period;
     TB0CCR1 = 250;
-    TB0CCR2 = base_period;
+    TB0CCR2 = TB0R + 50000;       // First trigger after ~50ms
 
     TB0CCTL0 &= ~CCIFG;
     TB0CCTL0 |= CCIE;
@@ -92,7 +117,7 @@ int main(void)
     //-----------------------------------------------------------------------
     // Bluetooth UART Initialization
     //-----------------------------------------------------------------------
-    UCA0CTLW0 |= UCSWRST;
+ /*   UCA0CTLW0 |= UCSWRST;
 
     UCA0CTLW0 |= UCSSEL__SMCLK;
     //UCA0BRW = 26;                          // Divider for 38400 baud at 1 MHz
@@ -110,18 +135,19 @@ int main(void)
 
     UCA0CTLW0 &= ~UCSWRST;
 
-
+*/
     //-----------------------------------------------------------------------
     // End Bluetooth UART Initialization
     //-----------------------------------------------------------------------
 
+
+
     PM5CTL0 &= ~LOCKLPM5;                       // Turn on gpio
     __enable_interrupt();
 
-
+    init_i2c_b0();
 
     while(1){
-        for(j=0; j<3000; j=j+1){};
 
         ADCCTL0 &= ~ADCENC;
         ADCMCTL0 = ADCINCH_0;
@@ -148,13 +174,15 @@ int main(void)
         while((ADCIFG & ADCIFG0) == 00);
         ADC_four = ADCMEM0;
 
-        // send UART transmission after ADC reads
+
+
+ /*       // send UART transmission after ADC reads
         uart_send_flag = true;
 
         // Schedule the compare event (must be > TB0R)
         TB0CCR2 = TB0R + 100;        // Schedule ~100 clock cycles in future
         TB0CCTL2 = CCIE;             // Enable interrupt
-
+*/
         // Detect direction and compute difference
         if (ADC_one > ADC_two) {
             left_right_max = ADC_one;
@@ -201,12 +229,22 @@ int main(void)
             TB0CCR1 = (unsigned int)control_raw;
         }
 
-
+        send_ADC();
     }
 
     return 0;
 }
 
+void send_ADC(void) {
+    Data_Cnt = 0;
+    Packet[0] = (ADC_three >> 8) & 0xFF;  // ADC_three MSB
+    Packet[1] = ADC_three & 0xFF;         // ADC_three LSB
+    Packet[2] = (ADC_four >> 8) & 0xFF;   // ADC_four MSB
+    Packet[3] = ADC_four & 0xFF;          // ADC_four LSB
+    UCB0TBCNT = sizeof(Packet);
+    UCB0CTLW0 |= UCTXSTT;                               // start condition
+    __delay_cycles(50000);
+}
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void motor_pwm_base_period(void){
 
@@ -232,19 +270,30 @@ __interrupt void timer_b1_isr(void) {
             P3OUT &= ~(BIT6 | BIT7);
             break;
 
-        case TB0IV_TBCCR2:
-            // UART trigger after ADC read
-            if (uart_send_flag) {
-                const char *p = message;
-                while (*p != '\0') {
-                    while (!(UCA0IFG & UCTXIFG)); // wait for TX buffer
-                    UCA0TXBUF = *p++;
-                }
-                uart_send_flag = false;
-            }
-            break;
+            Packet[0] = (ADC_three >> 8) & 0xFF;
+               Packet[1] = ADC_three & 0xFF;
+               Packet[2] = (ADC_four >> 8) & 0xFF;
+               Packet[3] = ADC_four & 0xFF;
+               Data_Cnt = 0;
+               UCB0TBCNT = sizeof(Packet);
+               UCB0CTLW0 |= UCTXSTT;  // Send I2C
+
+               // Schedule next event ~50ms later
+               TB0CCR2 += 50000;
+               break;
 
         default:
             break;
     }
 }
+#pragma vector=EUSCI_B0_VECTOR
+__interrupt void ADC_Send_ISR(void){
+    if (Data_Cnt == (sizeof(Packet) - 1)) {
+        UCB0TXBUF = Packet[Data_Cnt];
+        Data_Cnt = 0;
+    } else {
+        UCB0TXBUF = Packet[Data_Cnt];
+        Data_Cnt++;
+    }
+}
+
