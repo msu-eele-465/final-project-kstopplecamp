7df24d7c132be007c2caae38971bdfa5e8846cc5
kstopplecamp <49496979+kstopplecamp@users.noreply.github.com>
2025-05-08 15:08:44 -0600

    Keypad pin initialization

--- Calculator_code/Master_mcu/Final_Project_Calculator_Master/Calculator_master_main.c
+++ Calculator_code/Master_mcu/Final_Project_Calculator_Master/Calculator_master_main.c
@@ -10,3 +10,16 @@ int main(void)
 	
 	return 0;
 }
+void init_keypad(void) {
+    WDTCTL = WDTPW | WDTHOLD;                    // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;                        // Disable High Z mode
+
+    //--set up ports
+    P1DIR |=  (BIT4   |   BIT5   |   BIT6   |   BIT7);  // Set keypad row pins as outputs
+    P1OUT |=  (BIT4   |   BIT5   |   BIT6   |   BIT7);  // sets output high to start
+
+    // Set column pins as input with pull-down resistor
+    P2DIR &= ~(BIT0   |   BIT1   |   BIT2   |   BIT3); // Set P1.4 - p1.7 as input
+    P2REN |=  (BIT0   |   BIT1   |   BIT2   |   BIT3); // Enable pull-up/down resistors
+    P2OUT &= ~(BIT0   |   BIT1   |   BIT2   |   BIT3); // Set as pull-down
+}
